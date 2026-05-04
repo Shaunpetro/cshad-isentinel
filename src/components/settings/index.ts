45e64864db3004559999f03a499d851114da8c81
Shaunpetro <shaun.p.mckoy@gmail.com>
2026-05-04 13:32:24 +0200

    fix: add FeedbackModal export to settings barrel

--- src/components/settings/index.ts
+++ src/components/settings/index.ts
@@ -1,8 +1,7 @@
-// src/components/settings/index.ts
 export { LegalModal } from './LegalModal';
 export { DeveloperCredits } from './DeveloperCredits';
 export { SettingsItem } from './SettingsItem';
 export { PreferenceToggle } from './PreferenceToggle';
 export { PreferenceSelector } from './PreferenceSelector';
-export { OptionPickerModal } from './OptionPickerModal';
-export type { PickerOption } from './OptionPickerModal';
+export { OptionPickerModal, type PickerOption } from './OptionPickerModal';
+export { FeedbackModal } from './FeedbackModal';
