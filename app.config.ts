1aac5d227195ec985d8fbaf5d47823032f875597
Shaunpetro <shaun.p.mckoy@gmail.com>
2026-06-30 11:05:04 +0200

    first production app.config version code 9

--- app.config.ts
+++ app.config.ts
@@ -39,7 +39,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#1C1C1C",
     },
     package: "cshad.isentinel.news",
-    versionCode: 6,
+    versionCode: 9,
     googleServicesFile: "./google-services.json",
     config: {
       googleMaps: {
