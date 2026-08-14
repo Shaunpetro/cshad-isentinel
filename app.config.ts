561774b26447c8349944d4803ff7c02e72fdefa4
Shaunpetro <shaun.p.mckoy@gmail.com>
2026-08-14 07:55:56 +0200

    chore: bump production version to 3.2608.01, versionCode 15

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => {
   const expoConfig: ExpoConfig = {
     name: "CSHAD iSentinel News",
-    slug: "cshadnews",
+    slug: process.env.EAS_SLUG || "cshadnews",
     owner: process.env.EAS_OWNER || "shaunatg",
     version: "3.2608.01",
     runtimeVersion: "3.2608.01",
@@ -70,7 +70,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
 
     updates: {
-      url: "https://u.expo.dev/ce9ad511-1168-4d71-941d-35a5f4214892",
+      url: "https://u.expo.dev/41d9d284-b014-48ab-9238-fe2c0724fd98",
       checkAutomatically: "ON_LOAD",
       fallbackToCacheTimeout: 3000,
     },
@@ -81,7 +81,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       buildDate: new Date().toISOString(),
       developer: "Petro@ATG",
       eas: {
-        projectId: process.env.EAS_PROJECT_ID || "ce9ad511-1168-4d71-941d-35a5f4214892",
+        projectId: process.env.EAS_PROJECT_ID || "41d9d284-b014-48ab-9238-fe2c0724fd98",
       },
     },
   };
